e0c472a9d0e5e917a2a669af3c673b31c442765e
epte131988 <60382660+epte131988@users.noreply.github.com>
2021-09-21 23:12:15 +0300

    Add files via upload

---
+++ js/corusel.js
@@ -0,0 +1,8 @@
+$(function() {
+    $('.rotating-slider').rotatingSlider({
+        slideHeight : 320,
+        slideWidth : 300,
+        autoRotate: true, 
+        autoRotateInterva: 1000
+    });
+})
